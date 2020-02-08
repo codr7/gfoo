2906c606aa6c5cae3a88ef6f2dfec2135bcd188c
codr7 <andreas.gone.wild@gmail.com>
2020-02-08 15:22:22 +0100

    extracted types

--- pkg/macro.go
+++ pkg/macro.go
@@ -1,32 +1,7 @@
 package gfoo
 
 import (
-	"fmt"
-	"io"
 )
-
-var TMacro MacroType
-
-func init() {
-	TMacro.Init("Macro")
-}
-
-type MacroType struct {
-	TypeBase
-}
-
-func (_ *MacroType) Compare(x, y interface{}) Order {
-	return CompareString(x.(*Macro).name, y.(*Macro).name)
-}
-
-func (_ *MacroType) Dump(val interface{}, out io.Writer) error {
-	_, err := fmt.Fprintf(out, "Macro(%v)", val.(*Macro).name)
-	return err
-}
-
-func (self *MacroType) Unquote(pos Pos, val interface{}) Form {
-	return NewLiteral(pos, self, val)
-}
 
 type MacroImp = func(gfoo *GFoo, scope *Scope, form Form, args *Forms, out []Op) ([]Op, error)
 
