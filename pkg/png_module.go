18d0e8742673eb76352860288357c1bda2b72869
codr7 <codr7@outlook.com>
2020-03-19 03:30:46 +0100

    added encode

--- pkg/png_module.go
+++ pkg/png_module.go
@@ -1,10 +1,22 @@
 package gfoo
 
+import (
+	"image/png"
+	"io"
+)
+
 type PngModule struct {
 	Scope
 }
 
+func pngEncode(scope *Scope, stack *Slice, pos Pos) error {
+	out := stack.Pop().data.(io.Writer)
+	image := stack.Pop().data.(*Rgba)
+	return png.Encode(out, image)
+}
+
 func (self *PngModule) Init() *Scope {
 	self.Scope.Init()
+	self.AddMethod("encode", []Arg{AType("image", &TRgba), AType("out", &TWriter)}, nil, pngEncode)
 	return &self.Scope
 }
