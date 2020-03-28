104669e24a492c6fda8f573f79dec6bb5c670c9c
codr7 <codr7@outlook.com>
2020-03-28 21:52:29 +0100

    string tests

--- pkg/string_module.go
+++ pkg/string_module.go
@@ -8,6 +8,20 @@ type StringModule struct {
 	Module
 }
 
+func charChainImp(thread *Thread, registers, stack *Slice, pos Pos) error {
+	y := stack.Pop().data.(rune)
+	x := stack.Pop().data.(rune)
+	stack.Push(NewVal(&TString, string([]rune{x, y})))
+	return nil
+}
+
+func stringChainImp(thread *Thread, registers, stack *Slice, pos Pos) error {
+	y := stack.Pop().data.(string)
+	x := stack.Pop().data.(string)
+	stack.Push(NewVal(&TString, string(append([]rune(x), []rune(y)...))))
+	return nil
+}
+
 func stringDownImp(thread *Thread, registers, stack *Slice, pos Pos) error {
 	stack.Push(NewVal(&TString, strings.ToLower(stack.Pop().data.(string))))
 	return nil
@@ -21,6 +35,13 @@ func stringUpImp(thread *Thread, registers, stack *Slice, pos Pos) error {
 func (self *StringModule) Init() *Module {
 	self.Module.Init()
 	
+	self.AddMethod("~", []Arg{AType("x", &TChar), AType("y", &TChar)}, []Ret{RType(&TString)}, charChainImp)
+
+	self.AddMethod("~",
+		[]Arg{AType("x", &TString), AType("y", &TString)},
+		[]Ret{RType(&TString)},
+		stringChainImp)
+
 	self.AddMethod("down", []Arg{AType("in", &TString)}, []Ret{RType(&TString)}, stringDownImp)
 	self.AddMethod("up", []Arg{AType("in", &TString)}, []Ret{RType(&TString)}, stringUpImp)
 
